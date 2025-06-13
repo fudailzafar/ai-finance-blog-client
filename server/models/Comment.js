b66b91234236f98adb7418872979cac514452fbc
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-06-13 15:31:45 +0530

    Created comments API for blog posts

---
+++ server/models/Comment.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    blog: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "aiblog",
+      required: true,
+    },
+    name: { type: String, required: true },
+    content: { type: String, required: true },
+    isApproved: { type: SVGAnimatedBoolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   addBlog,
+  addComment,
   deleteBlogById,
   getAllBlogs,
   getBlogById,
+  getBlogComments,
   togglePublish,
 } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
@@ -15,6 +17,8 @@ blogRouter.post("/add", upload.single("image"), auth, addBlog);
 blogRouter.get("/all", getAllBlogs);
 blogRouter.get("/:blogId", getBlogById);
 blogRouter.post("/delete", auth, deleteBlogById);
-blogRouter.post("/toggle-publish", auth, togglePublish)
+blogRouter.post("/toggle-publish", auth, togglePublish);
+blogRouter.post("/add-comment", addComment)
+blogRouter.post("/comments", getBlogComments)
 
 export default blogRouter;
